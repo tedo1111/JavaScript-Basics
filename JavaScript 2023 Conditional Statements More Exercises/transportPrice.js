8b28978f254f74d2a31b90f0113d8e14b7e99e1a
edo1111` <edo1998@abv.bg`>
2023-11-19 21:00:37 +0200

    Ex. 4

--- JavaScript 2023 Conditional Statements More Exercises/transportPrice.js
+++ JavaScript 2023 Conditional Statements More Exercises/transportPrice.js
@@ -1,6 +1,34 @@
-function transportPrice() {
+function transportPrice(input) {
 
+    let km = Number(input[0]);
+    let dayNight = input[1];
 
+    let price = 0;
+
+    if (km < 20) {
+        if (dayNight === `day`) {
+            price = km * 0.79 + 0.7;
+        }
+        else {
+            price = km * 0.9 + 0.7;
+        }
+    }
+    else if (km >= 20 && km < 100) {
+        if (dayNight === `day` || dayNight === `night`) {
+            price = km * 0.09;
+        }
+    }
+
+    else if (km >= 100) {
+        if (dayNight === `day` || dayNight === `night`) {
+            price = km * 0.06;
+        }
+    }
+
+    console.log(price.toFixed(2));
 
 }
-transportPrice([]);
+transportPrice(["5", "day"]);
+transportPrice(["7", "night"]);
+transportPrice(["25", "day"]);
+transportPrice(["180", "night"]);
