2600fb463b1effa110ba2a139b38af72d5e43b83
edo1111` <edo1998@abv.bg`>
2023-12-05 00:49:49 +0200

    Ex. 8

--- JavaScript 2023 Conditional Statements Advanced More Exercises/pointOnRectangleBorder.js
+++ JavaScript 2023 Conditional Statements Advanced More Exercises/pointOnRectangleBorder.js
@@ -1,5 +1,35 @@
 function pointOnRectangleBorder(input) {
 
+    let x1 = Number(input[0]);
+    let y1 = Number(input[1]);
+    let x2 = Number(input[2]);
+    let y2 = Number(input[3]);
+    let x = Number(input[4]);
+    let y = Number(input[5]);
+
+
+    if (x === x1 || x === x2) {
+        if (y >= y1 && y <= y2) {
+            console.log(`Border`);
+        }
+        else {
+            console.log(`Inside / Outside`);
+        }
+    }
+    else if (y === y1 || y === y2) {
+        if (x >= x1 && x <= x2) {
+            console.log(`Border`);
+        }
+        else {
+            console.log(`Inside / Outside`);
+        }
+    }
+    else {
+        console.log(`Inside / Outside`);
+    }
+
 }
+
 pointOnRectangleBorder(["2", "-3", "12", "3", "8", "-1"]);
 pointOnRectangleBorder(["2", "-3", "12", "3", "12", "-1"]);
+pointOnRectangleBorder(["2", "-3", "12", "3", "2", "3"]);
